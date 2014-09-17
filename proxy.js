95ccaba889a1b71dde68b1b6b9dc1a33e33796c9
Anton Chebotaev <anton@talkto.io>
2014-09-17 22:29:57 +0400

    added argument for list of supported plugins

--- proxy.js
+++ proxy.js
@@ -13,6 +13,7 @@ function usage() {
   console.log('  --influxdb_user : influxdb user');
   console.log('  --influxdb_password : influxdb password');
   console.log('  --verbose : display metric name pushed into influxdb');
+  console.log('  --collectd_plugins : comma-separated list of allowed plugins, leave blank to allow all');
   console.log('  --help : this help');
   process.exit(1);
 }
@@ -52,6 +53,12 @@ if (!argv.influxdb_password) {
   usage();
 }
 
+var collectdPluginEnabled = function(plugin) {
+  var argumentMissing = !argv.collectd_plugins
+  var pluginEnabled = !!argv.collectd_plugins && (argv.collectd_plugins.split(",").indexOf(plugin) >= 0)
+  return argumentMissing || pluginEnabled
+}
+
 argv.influxdb_path = '/db/' + argv.influxdb_db + '/series?u=' + argv.influxdb_user + '&p=' + argv.influxdb_password + '&time_precision=s';
 console.log('Influx db config');
 console.log('Host :', argv.influxdb_host, ':', argv.influxdb_port);
@@ -75,29 +82,36 @@ var server = http.createServer(function(req, res) {
       // then append plugin name (like "cpu" or "memory")
       var metricPrefix = 'collectd.' + dataElement.plugin;
 
-      // and then apply plugin's internal names
-      if (dataElement.plugin_instance !== '') {
-        metricPrefix = metricPrefix + '.' + dataElement.plugin_instance;
-      }
-      metricPrefix = metricPrefix + '.' + dataElement.type;
-      if (dataElement.type_instance !== '') {
-        metricPrefix = metricPrefix + '.' + dataElement.type_instance;
-      }
+      if (collectdPluginEnabled(dataElement.plugin)) {
 
-      // for each dstype add separate metric
-      for(var dstype in dataElement.dstypes) {
-        var isCounterOrGauge = dataElement.dstypes[dstype] == 'counter' || dataElement.dstypes[dstype] == 'gauge'
-        if (isCounterOrGauge) {
-          var metricName = metricPrefix + '.' + dataElement.dsnames[dstype];
-          if (argv.verbose) {
-            console.log('Push metric', metricName);
+        // then apply plugin's internal names
+        if (dataElement.plugin_instance !== '') {
+          metricPrefix = metricPrefix + '.' + dataElement.plugin_instance;
+        }
+        metricPrefix = metricPrefix + '.' + dataElement.type;
+        if (dataElement.type_instance !== '') {
+          metricPrefix = metricPrefix + '.' + dataElement.type_instance;
+        }
+
+        // for each dstype add separate metric
+        for(var dstype in dataElement.dstypes) {
+          var metricTypeSupported =
+              dataElement.dstypes[dstype] == 'counter' ||
+              dataElement.dstypes[dstype] == 'gauge' ||
+              dataElement.dstypes[dstype] == 'derive'
+
+          if (metricTypeSupported) {
+            var metricName = metricPrefix + '.' + dataElement.dsnames[dstype];
+            if (argv.verbose) {
+              console.log('Push metric', metricName);
+            }
+            var value = dataElement.values[dstype]
+            output.push({
+              name: metricName,
+              columns: ['time', 'value', 'hostname'],
+              points: [[dataElement.time, value, hostname]],
+            });
           }
-          var value = dataElement.values[dstype]
-          output.push({
-            name: metricName,
-            columns: ['time', 'value', 'hostname'],
-            points: [[dataElement.time, value, hostname]],
-          });
         }
       }
     });
